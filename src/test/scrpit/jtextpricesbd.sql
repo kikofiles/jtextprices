22d2172f0f6cbc38cd8654b66a9f8d2d41b36a32
kikofiles <34756284+kikofiles@users.noreply.github.com>
2017-12-22 14:38:43 -0600

    Primera version Base de Datos

---
+++ src/test/scrpit/jtextpricesbd.sql
@@ -0,0 +1,207 @@
+/* MySQL Workbench Forward Engineering */
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+DROP DATABASE IF EXISTS jtextpricesbd;
+
+CREATE DATABASE jtextpricesbd
+    CHARACTER SET 'latin1'
+    COLLATE 'latin1_spanish_ci';
+
+USE jtextpricesbd;
+
+/* Dropping database objects */
+
+DROP TABLE IF EXISTS precioproducto;
+DROP TABLE IF EXISTS tienda;
+DROP TABLE IF EXISTS logprecioproducto;
+DROP TABLE IF EXISTS tipoprecio;
+DROP TABLE IF EXISTS producto;
+DROP TABLE IF EXISTS clasificaciontienda;
+DROP TABLE IF EXISTS acceso;
+DROP TABLE IF EXISTS usuario;
+DROP TABLE IF EXISTS rol;
+
+/* Structure for the `rol` table :  */
+
+CREATE TABLE rol (
+  IDRol INTEGER(3) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Identificador del Rol',
+  NombreRol VARCHAR(20) COLLATE latin1_spanish_ci NOT NULL COMMENT 'Nombre del Rol',
+  PRIMARY KEY USING BTREE (IDRol),
+  UNIQUE KEY IDRol USING BTREE (IDRol),
+  UNIQUE KEY NombreRol USING BTREE (NombreRol)
+) ENGINE=InnoDB
+AUTO_INCREMENT=3 ROW_FORMAT=DYNAMIC CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci'
+;
+
+/* Structure for the `usuario` table :  */
+
+CREATE TABLE usuario (
+  IDUsuario INTEGER(6) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Identificador Unico del Usuario',
+  NickName VARCHAR(20) COLLATE latin1_spanish_ci NOT NULL COMMENT 'Nombre de usuario en Sistema',
+  Password VARCHAR(20) COLLATE latin1_spanish_ci NOT NULL COMMENT 'Contraseña del usuario',
+  Nombre VARCHAR(40) COLLATE latin1_spanish_ci NOT NULL COMMENT 'Nombre Real del Usuario',
+  Estatus INTEGER(1) UNSIGNED NOT NULL DEFAULT 1 COMMENT 'Activo',
+  PRIMARY KEY USING BTREE (IDUsuario),
+  UNIQUE KEY IDUsuario USING BTREE (IDUsuario),
+  UNIQUE KEY NickName USING BTREE (NickName)
+) ENGINE=InnoDB
+AUTO_INCREMENT=1 ROW_FORMAT=DYNAMIC CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci'
+COMMENT='Informacion sobre el usuario que accesa al Sistema'
+;
+
+/* Structure for the `acceso` table :  */
+
+CREATE TABLE acceso (
+  IDAcceso INTEGER(3) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Identificador por tipo de Acceso',
+  usuario_IDUsuario INTEGER(6) UNSIGNED NOT NULL,
+  rol_IDRol INTEGER(3) UNSIGNED NOT NULL,
+  PRIMARY KEY USING BTREE (IDAcceso, usuario_IDUsuario, rol_IDRol),
+  UNIQUE KEY IDAcceso USING BTREE (IDAcceso),
+  KEY fk_acceso_usuario_idx USING BTREE (usuario_IDUsuario),
+  KEY fk_acceso_rol1_idx USING BTREE (rol_IDRol),
+  CONSTRAINT fk_acceso_rol1 FOREIGN KEY (rol_IDRol) REFERENCES rol (IDRol) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT fk_acceso_usuario FOREIGN KEY (usuario_IDUsuario) REFERENCES usuario (IDUsuario) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB
+AUTO_INCREMENT=1 ROW_FORMAT=DYNAMIC CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci'
+;
+
+/* Structure for the `clasificaciontienda` table :  */
+
+CREATE TABLE clasificaciontienda (
+  IDClasificacionTienda INTEGER(1) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Tipo de Tienda\r\n1 Matriz, 2 Sucursal',
+  NombreClasificacionTienda VARCHAR(30) COLLATE latin1_spanish_ci NOT NULL COMMENT 'Matriz ó Sucursal',
+  Estatus INTEGER(1) UNSIGNED NOT NULL COMMENT 'Activo',
+  PRIMARY KEY USING BTREE (IDClasificacionTienda),
+  UNIQUE KEY IDClasificacionTienda USING BTREE (IDClasificacionTienda),
+  UNIQUE KEY NombreClasificacionTienda USING BTREE (NombreClasificacionTienda)
+) ENGINE=InnoDB
+AUTO_INCREMENT=3 ROW_FORMAT=DYNAMIC CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci'
+;
+
+/* Structure for the `producto` table :  */
+
+CREATE TABLE producto (
+  IDProducto INTEGER(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Identificacion de Producto',
+  `Nombre Producto` VARCHAR(40) COLLATE latin1_spanish_ci NOT NULL COMMENT 'Nombre del Producto',
+  Ancho DOUBLE(6,2) UNSIGNED DEFAULT NULL COMMENT 'El ancho de la tela',
+  Rendimiento DOUBLE(6,2) UNSIGNED DEFAULT NULL COMMENT 'Metros para 1Kg',
+  Estatus INTEGER(1) UNSIGNED NOT NULL DEFAULT 1 COMMENT 'Producto Activo',
+  PRIMARY KEY USING BTREE (IDProducto),
+  UNIQUE KEY IDProducto USING BTREE (IDProducto)
+) ENGINE=InnoDB
+AUTO_INCREMENT=1 ROW_FORMAT=DYNAMIC CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci'
+;
+
+/* Structure for the `tipoprecio` table :  */
+
+CREATE TABLE tipoprecio (
+  IDTipoPrecio INTEGER(1) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Por Rollo, por Kg o por Metros',
+  NombrePrecio VARCHAR(20) COLLATE latin1_spanish_ci NOT NULL COMMENT 'La Descripcion del Precio',
+  PRIMARY KEY USING BTREE (IDTipoPrecio),
+  UNIQUE KEY IDTipoPrecio USING BTREE (IDTipoPrecio)
+) ENGINE=InnoDB
+AUTO_INCREMENT=4 ROW_FORMAT=DYNAMIC CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci'
+;
+
+/* Structure for the `logprecioproducto` table :  */
+
+CREATE TABLE logprecioproducto (
+  IDLog INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Identificador por transacción de cambio de precio',
+  FechaLog TIMESTAMP NULL DEFAULT NULL,
+  usuario_IDUsuario INTEGER(6) UNSIGNED NOT NULL,
+  producto_IDProducto INTEGER(8) UNSIGNED NOT NULL,
+  tipoprecio_IDTipoPrecio INTEGER(1) UNSIGNED NOT NULL,
+  PrecioPrevio DOUBLE(8,2) UNSIGNED NOT NULL COMMENT 'El Precio Anterior al cambio',
+  NuevoPrecio DOUBLE(8,2) UNSIGNED NOT NULL COMMENT 'El nuevo precio registrado',
+  PRIMARY KEY USING BTREE (IDLog, usuario_IDUsuario, producto_IDProducto, tipoprecio_IDTipoPrecio),
+  UNIQUE KEY IDLog USING BTREE (IDLog),
+  KEY fk_logprecioproducto_usuario1_idx USING BTREE (usuario_IDUsuario),
+  KEY fk_logprecioproducto_producto1_idx USING BTREE (producto_IDProducto),
+  KEY fk_logprecioproducto_tipoprecio1_idx USING BTREE (tipoprecio_IDTipoPrecio),
+  CONSTRAINT fk_logprecioproducto_producto1 FOREIGN KEY (producto_IDProducto) REFERENCES producto (IDProducto) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT fk_logprecioproducto_tipoprecio1 FOREIGN KEY (tipoprecio_IDTipoPrecio) REFERENCES tipoprecio (IDTipoPrecio) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT fk_logprecioproducto_usuario1 FOREIGN KEY (usuario_IDUsuario) REFERENCES usuario (IDUsuario) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB
+AUTO_INCREMENT=1 ROW_FORMAT=DYNAMIC CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci'
+;
+
+/* Structure for the `tienda` table :  */
+
+CREATE TABLE tienda (
+  IDTienda INTEGER(2) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'IDentificador por Tienda',
+  `Nombre Tienda` VARCHAR(30) COLLATE latin1_spanish_ci NOT NULL COMMENT 'El Nombre de la Tienda',
+  clasificaciontienda_IDClasificacionTienda INTEGER(1) UNSIGNED NOT NULL,
+  Estatus INTEGER(1) UNSIGNED NOT NULL COMMENT 'Activo',
+  PRIMARY KEY USING BTREE (IDTienda, clasificaciontienda_IDClasificacionTienda),
+  UNIQUE KEY IDTienda USING BTREE (IDTienda),
+  KEY fk_tienda_clasificaciontienda1_idx USING BTREE (clasificaciontienda_IDClasificacionTienda),
+  CONSTRAINT fk_tienda_clasificaciontienda1 FOREIGN KEY (clasificaciontienda_IDClasificacionTienda) REFERENCES clasificaciontienda (IDClasificacionTienda) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB
+AUTO_INCREMENT=6 ROW_FORMAT=DYNAMIC CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci'
+COMMENT='\t'
+;
+
+/* Structure for the `precioproducto` table :  */
+
+CREATE TABLE precioproducto (
+  IDPrecioProducto INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'El Precio por el Producto',
+  producto_IDProducto INTEGER(8) UNSIGNED NOT NULL,
+  tipoprecio_IDTipoPrecio INTEGER(1) UNSIGNED NOT NULL,
+  PrecioProducto DOUBLE(8,2) UNSIGNED NOT NULL COMMENT 'El precio correspondiente del Producto',
+  tienda_IDTienda INTEGER(2) UNSIGNED NOT NULL,
+  FechaAlta TIMESTAMP NULL DEFAULT NULL,
+  usuario_IDUsuarioAlta INTEGER(6) UNSIGNED NOT NULL,
+  usuario_IDUsuarioModifica INTEGER(6) UNSIGNED NOT NULL,
+  FechaMod TIMESTAMP NULL DEFAULT NULL,
+  Estatus INTEGER(1) NOT NULL COMMENT 'Activo?',
+  PRIMARY KEY USING BTREE (IDPrecioProducto, producto_IDProducto, tipoprecio_IDTipoPrecio, tienda_IDTienda, usuario_IDUsuarioAlta, usuario_IDUsuarioModifica),
+  UNIQUE KEY IDPrecioProducto USING BTREE (IDPrecioProducto),
+  KEY fk_precioproducto_producto1_idx USING BTREE (producto_IDProducto),
+  KEY fk_precioproducto_tipoprecio1_idx USING BTREE (tipoprecio_IDTipoPrecio),
+  KEY fk_precioproducto_tienda1_idx USING BTREE (tienda_IDTienda),
+  KEY fk_precioproducto_usuario1_idx USING BTREE (usuario_IDUsuarioAlta),
+  KEY fk_precioproducto_usuario2_idx USING BTREE (usuario_IDUsuarioModifica),
+  CONSTRAINT fk_precioproducto_producto1 FOREIGN KEY (producto_IDProducto) REFERENCES producto (IDProducto) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT fk_precioproducto_tienda1 FOREIGN KEY (tienda_IDTienda) REFERENCES tienda (IDTienda) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT fk_precioproducto_tipoprecio1 FOREIGN KEY (tipoprecio_IDTipoPrecio) REFERENCES tipoprecio (IDTipoPrecio) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT fk_precioproducto_usuario1 FOREIGN KEY (usuario_IDUsuarioAlta) REFERENCES usuario (IDUsuario) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT fk_precioproducto_usuario2 FOREIGN KEY (usuario_IDUsuarioModifica) REFERENCES usuario (IDUsuario) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB
+AUTO_INCREMENT=1 ROW_FORMAT=DYNAMIC CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci'
+;
+
+/* Data for the `rol` table  (LIMIT 0,500) */
+
+INSERT INTO `rol` (IDRol, NombreRol) VALUES
+  (1,'Administrador'),
+  (2,'Consultor');
+COMMIT;
+
+/* Data for the `clasificaciontienda` table  (LIMIT 0,500) */
+
+INSERT INTO `clasificaciontienda` (IDClasificacionTienda, NombreClasificacionTienda, Estatus) VALUES
+  (1,'Matriz',1),
+  (2,'Sucursal',1);
+COMMIT;
+
+/* Data for the `tipoprecio` table  (LIMIT 0,500) */
+
+INSERT INTO `tipoprecio` (IDTipoPrecio, NombrePrecio) VALUES
+  (1,'Por Rollo'),
+  (2,'Por Kilo'),
+  (3,'Por Metro');
+COMMIT;
+
+/* Data for the `tienda` table  (LIMIT 0,500) */
+
+INSERT INTO `tienda` (IDTienda, `Nombre Tienda`, clasificaciontienda_IDClasificacionTienda, Estatus) VALUES
+  (1,'Centro',1,1),
+  (2,'Chiconcuac',2,1),
+  (3,'Moroleón',2,1),
+  (4,'Zapotlanejo',2,1),
+  (5,'Cruces',2,1);
+COMMIT;
+

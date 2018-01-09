0aca20a8211d6820037f88353c96a94f9f27ccab
kikofiles <34756284+kikofiles@users.noreply.github.com>
2018-01-08 18:07:39 -0600

    Inicio control y alta usuario

--- src/main/java/com/quimera/corptextiljt/zk/usuario/ControlAltaUsuario.java
+++ src/main/java/com/quimera/corptextiljt/zk/usuario/ControlAltaUsuario.java
@@ -1,15 +1,14 @@
 package com.quimera.corptextiljt.zk.usuario;
 
-import java.util.List;
 
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Listcell;
-import org.zkoss.zul.Listitem;
+
+
 import org.zkoss.zul.Window;
 import org.zkoss.zul.Textbox;
-import org.zkoss.zul.Decimalbox;
-import org.zkoss.zul.Checkbox;
 import org.zkoss.zul.Combobox;
+
+import java.math.BigDecimal;
+
 import org.zkoss.zul.Button;
 
 import com.quimera.corptextiljt.model.Precioproducto;
@@ -18,7 +17,7 @@ import com.quimera.corptextiljt.service.ServiceProducto;
 import com.quimera.corptextiljt.service.ServicePrecioProducto;
 import com.quimera.corptextiljt.service.context.SpringApplicationContext;
 import com.quimera.corptextiljt.util.Constants;
-import java.math.BigDecimal;
+
 
 
 
@@ -51,5 +50,39 @@ public class ControlAltaUsuario extends Window{
 		idBtnCancelar = (Button) getFellow("idBotonCancelar");
 		idBtnGuardar = (Button) getFellow("idBotonGuardar");
 	}
+	
+	
+	// Proceso de almacenamiento de Usuario
+	public void guardaUsuario() {
+		System.out.println("Entre a Guarda Usuario...");
+		// Primero checo que hayan puesto el nombre Completo del Usuario
+		if(idTxtNombreUsuario.getValue().trim() != null && idTxtNombreUsuario.getValue().trim().length()>0) {
+			// Segundo checo los valores de Nickname y Password
+			if(idTxtApodo.getValue() != null && (idTxtApodo.getValue().trim().length()>0) 
+				&& idTxtContra.getValue() != null && (idTxtContra.getValue().trim().length()>0)) {
+				// Checo que los valores de mis combobox no sean nulos
+				if(idCmbTipoUsuario.getSelectedIndex() != -1 && idCmbTiendaAsignada.getSelectedIndex() != -1) {
+					// Todo OK, puedo comenzar a guardar
+					
+					System.out.println("Valor En combo TipoUSuario: " +idCmbTipoUsuario.getValue()+", Index: "+idCmbTipoUsuario.getSelectedIndex() +", ID: " +idCmbTipoUsuario.getId());
+					System.out.println("Valor En combo Sitio Asignado: " +idCmbTiendaAsignada.getValue()+", Index: "+idCmbTiendaAsignada.getSelectedIndex() +", Item: " +idCmbTiendaAsignada.getSelectedItem().toString());
+					
+					
+				}
+				else {
+					// Regreso que no estan seleccionados las opciones en los combos.
+				}
+			}
+			else {
+				// Regreso Avisando que faltan el Nickname y/o el PAssword...
+			}
+		}
+		else {
+			// Regresar avisando que falta el nombre del Usuario
+		}		
+	}
+	
+	
+	
 	
 }
